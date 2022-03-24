ab3781eb986d10b00933a55fd98e2541bbd68a06
EnsePS5 <Kaaagodlewski@gmail.com>
2022-03-24 15:00:26 +0100

    Initial commit: Adding data and basic program body

--- NAIProject02_Perceptor/Program.cs
+++ NAIProject02_Perceptor/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace NAIProject02_Perceptor
 {
     public class Program
     {
+        static string trainingFilePath;
         public static void Main(string[] args)
-        {
+        { 
+
             //Introduction commands
-            Console.WriteLine("Proszę wprowadzić stałą uczenia(int): ");
+            Console.WriteLine("Proszę wprowadzić stałą uczenia(int+): ");
             int learningCons = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Proszę wprowadzić ścieżkę do pliku treningowego: ");
-            string traningFilePath = Console.ReadLine();
+            trainingFilePath = Console.ReadLine();
+
+            if (!File.Exists(trainingFilePath)) 
+                throw new Exception("Plik nieistnieje.");
+
 
 
             //Learning process in loop
             string anwser = string.Empty;
-
             Perceptor perceptor = Perceptor.GetInstance();
 
             do
@@ -30,6 +36,17 @@ namespace NAIProject02_Perceptor
                 anwser = Console.ReadLine();
 
             } while (anwser == "Y");
+        }
+
+        //file data extraction method
+        private void dataReader(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+
+            foreach (string line in lines)
+            {
+                //TODO do smth
+            }
         }
     }
 }
