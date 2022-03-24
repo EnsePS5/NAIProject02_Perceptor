12ab1a50c1bb2b9d76e382578e2f78c61f939e6e
EnsePS5 <Kaaagodlewski@gmail.com>
2022-03-24 13:06:47 +0100

    Dodaj pliki projektów.

---
+++ NAIProject02_Perceptor/Perceptor.cs
@@ -0,0 +1,30 @@
+﻿namespace NAIProject02_Perceptor
+{
+    internal sealed class Perceptor
+    {
+        private Perceptor()
+        {
+
+        }
+
+        private static Perceptor _instance;
+
+        public static Perceptor GetInstance()
+        {
+            if (_instance == null)
+            _instance = new Perceptor();
+
+            return _instance;
+        }
+
+        public void Learn()
+        {
+
+        }
+
+        public string Assign()
+        {
+            return "";
+        }
+    }
+}
---
+++ NAIProject02_Perceptor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NAIProject02_Perceptor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Introduction commands
+            Console.WriteLine("Proszę wprowadzić stałą uczenia(int): ");
+            int learningCons = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Proszę wprowadzić ścieżkę do pliku treningowego: ");
+            string traningFilePath = Console.ReadLine();
+
+
+            //Learning process in loop
+            string anwser = string.Empty;
+
+            Perceptor perceptor = Perceptor.GetInstance();
+
+            do
+            {
+
+                Console.WriteLine("Rozpoczęto proces nauczania.");
+
+                
+
+                Console.WriteLine("Zakończono proces nauczania. Czy chcesz powtórzyć nauczanie? Y/N: ");
+                anwser = Console.ReadLine();
+
+            } while (anwser == "Y");
+        }
+    }
+}
